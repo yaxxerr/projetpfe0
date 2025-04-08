a3027a3a5c718dcbff80c9d7f87078e69f873f30
yaxxerr <134547361+yaxxerr@users.noreply.github.com>
2025-04-08 19:25:06 +0100

    admin files added

--- projetpfe/settings.py
+++ projetpfe/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ai',
+    'courses',
+    'users',
+    'programs',
+    'quizzes',
+    'ressources'
 ]
 
 MIDDLEWARE = [
