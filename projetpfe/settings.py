55019347982ffeb2b94b3f01095e4cb7bfcb5fb3
yaxxerr <134547361+yaxxerr@users.noreply.github.com>
2025-04-09 13:58:32 +0100

    database added , well configured

--- projetpfe/settings.py
+++ projetpfe/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'projetpfe.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'prjpfedb',
+        'USER': 'admin0',
+        'PASSWORD': 'admin0',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
