cdd7cd9e4aa0b29c2398940d828a55ddadfcede1
yaxxerr <134547361+yaxxerr@users.noreply.github.com>
2025-04-13 22:56:55 +0100

    online database

--- projetpfe/settings.py
+++ projetpfe/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -82,14 +82,9 @@ WSGI_APPLICATION = 'projetpfe.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'prjpfedb',
-        'USER': 'admin0',
-        'PASSWORD': 'admin0',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.parse(
+        "postgresql://neondb_owner:npg_KfUHvI63RTBN@ep-late-credit-a2s79lpq-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require"
+    )
 }
 
 
